375de0c3d9049ad704710f39f979af743c37395e
AlaaYlula <alaamwazani@hotmail.com>
2022-03-08 20:27:17 +0300

    Ch7

--- inheritance/lib/src/main/java/inheritance/Review.java
+++ inheritance/lib/src/main/java/inheritance/Review.java
@@ -1,16 +1,25 @@
 package inheritance;
 
-public class Review {
-    String body;
-    String author;
-    int stars;
+public class Review  {
+   private String body;
+    private String author;
+    private double stars;
 
-    public Review(String body, String author, int stars) {
+    private String movieName;
+
+    public Review( String author ,String body,double stars) { // Review For the Shop // Review For the Theatre // Resturant
         this.body = body;
         this.author = author;
         CheckStars(stars);
     }
-    public void CheckStars(int stars){
+
+    public Review( String author ,String body,double stars,String movieName) { // Review For the Theater
+        this.body = body;
+        this.author = author;
+        this.movieName = movieName;
+        CheckStars(stars);
+    }
+    public void CheckStars(double stars){
         if(stars <= 5 && stars > 0 ){
             this.stars = stars;
         }else {
@@ -34,7 +43,7 @@ public class Review {
         this.author = author;
     }
 
-    public int getStars() {
+    public double getStars() {
         return stars;
     }
 
@@ -42,11 +51,24 @@ public class Review {
         this.stars = stars;
     }
 
+    public String getMovieName() {
+        return movieName;
+    }
+
     @Override
     public String toString() {
+        if(this.movieName == null) {
+            return "Review{" +
+                    " author='" + author + '\'' +
+                    ", body='" + body + '\'' +
+                    ", stars=" + stars +
+                    '}';
+
+        }
         return "Review{" +
-                "body='" + body + '\'' +
+                "Movie Name='" + movieName + '\'' +
                 ", author='" + author + '\'' +
+                ", body='" + body + '\'' +
                 ", stars=" + stars +
                 '}';
     }
